9ca8a5e3518ee2f0335bded079e77287588f1d8e
psystar99 <soyoung99.park@gmail.com>
2021-10-24 21:48:44 +0900

    save request's json data in mongo

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 app.use(express.json()); // Update for Express 4.16+
-
 app.listen(8080,function(){
     console.log('listening on 8080')
 });
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/testDB');
+var db = mongoose.connection;
+db.on('error', function(){
+    console.log('Connection Failed!');
+});
+db.once('open', function() {
+    console.log('Connected!');
+});
+var student = mongoose.Schema({
+    name : 'string',
+    address : 'string',
+    age : 'number'
+});
+var Student = mongoose.model('Schema', student);
 //1. 어떠한 사용자에 대한 POST : 타이머 스탑할 때마다 CREATE......URL: /stopTimer 
 
 app.post('/stopTimer', function(request, response){
     console.log(request.body);      // your JSON
+    var newStudent = new Student(request.body);
+    newStudent.save(function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Data Saved!')
+        }
+    });
     response.send(request.body);    // echo the result back
+
   });
 
 //2. 어떠한 사용자에 대한 GET : 통계 페이지를 위한 READ....URL: /Statics/day?user_id=
