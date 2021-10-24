c3cc326dea8eeb49f303e25b6b898fb67a889f31
psystar99 <soyoung99.park@gmail.com>
2021-10-25 00:02:43 +0900

    GET: send json from mongo

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ var student = mongoose.Schema({
     age : 'number'
 });
 var Student = mongoose.model('Schema', student);
-//1. 어떠한 사용자에 대한 POST : 타이머 스탑할 때마다 CREATE......URL: /stopTimer 
 
+//1. 어떠한 사용자에 대한 POST : 타이머 스탑할 때마다 CREATE......URL: /stopTimer 
 app.post('/stopTimer', function(request, response){
     console.log(request.body);      // your JSON
     var newStudent = new Student(request.body);
@@ -33,13 +33,19 @@ app.post('/stopTimer', function(request, response){
             console.log('Data Saved!')
         }
     });
-    response.send(request.body);    // echo the result back
+});
 
-  });
-
-//2. 어떠한 사용자에 대한 GET : 통계 페이지를 위한 READ....URL: /Statics/day?user_id=
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+//2. 어떠한 사용자에 대한 GET : 통계 페이지를 위한 READ 그리고 클라언트에게 send 데이터....URL: /Statics/day?user_id=
+app.get('/statics',function(request, response){
+    Student.find(function(error, students){
+        console.log('--- Read all ---');
+        if(error){
+            console.log(error);
+        }else{
+            console.log(students);
+            response.send(students);    // echo the result back
+        }
+    })
 });
 
 //3. 어떠한 사용자에 대한 GET : 통계 페이지를 위한 READ
