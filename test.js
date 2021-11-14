56f30a4ea737ad557e3f3dd822a69ef4a2a63f6c
psystar99 <soyoung99.park@gmail.com>
2021-11-14 19:16:50 +0900

    mongoose update query and getToday date

--- test.js
+++ test.js
@@ -18,8 +18,8 @@ db.once('open', function() {
 });
 
 var focus = new mongoose.Schema({
-    type : 'string',
-    startTime : 'string',
+    type : 'string', //졺, 초점x
+    startTime : 'string', //1135
     endTime : 'string'
 })
 
@@ -29,79 +29,63 @@ var study = new mongoose.Schema({
 });
 
 var dayStudys = new mongoose.Schema({
-    day : 'Date',
+    day : 'string', //211114
     time : 'number',
     studys : [study],
     focusXs : [focus]//절대 시간 4자리 스트링인데 숫자. 1854
 });
 
+var student = mongoose.Schema({
+    studys : [study],
+    day : 'string'
+});
 
 //1. 어떠한 사용자에 대한 POST : 타이머 스탑할 때마다 CREATE......URL: /stopTimer 
 app.post('/stopTimer/:uid', function(request, response){
     //a. uid로 컬렉션과 연결
     var uid = request.params.uid;
+    console.log(request.body);
     console.log(uid + " 컬렉션에 연결하겠습니다.");
-    var UserDayStudys = mongoose.model(uid, dayStudys);
+    var Student = mongoose.model(uid, student);
     
-    // b. 오늘 날짜인 dayStudys 찾기
-    UserDayStudys.find({},function(error, userstuys){
-        if(error){// 없으므로 date는 오늘이고 study는 request.body인 JSON 새로 만들기
+    //b. 오늘 날짜 8자리로 변환하기
+    const day = new Date();
+    var today = day.getFullYear().toString()+(day.getMonth()+1).toString()+day.getDate().toString();
+    console.log(today);
 
-        }else{// date가 오늘인 
-
-        }
-    }) 
-
-    
-    var newStudent = new Student(request.body);
-    newStudent.save(function(error, data){
-        if(error){
-            console.log(error);
-        }else{
-          response.send('서버로부터의 메세지: Data Saved!')
-          console.log('Data Saved!')
-        }
-    });
+    // c. 오늘 날짜인 데이터 찾기- 있으면 배열 추가하기, 없으면 새로 만들기
+    var filterParam = {};
+    filterParam['day']=today;
 
     //c. 안드로이드에서 보낸 공부 구간(study) JSON을 오늘의 dayStudys의 studys에 추가
-
     //d. 누적 시간 update
+    var newStudy = request.body;
+    var updateParam = {studys: newStudy};
+
+    Student.findOneAndUpdate(filterParam, {$push: updateParam}, {
+        new: true,
+        upsert: true // Make this update into an upsert
+    }, function(err){
+        if(err){
+                console.log(err);
+        }else{
+                console.log("Successfully added");
+                response.send("잘 저장됨");
+        }
+    });
 });
 
 //2. 어떠한 사용자에 대한 GET : 통계 페이지를 위한 READ 그리고 클라언트에게 send 데이터....URL: /Statics/:uid/:day
-app.get('/statics/:uid/:day',function(request, response){
+app.get('/test/:uid/:day',function(request, response){
 
     //a. uid로 컬렉션과 연결
     var uid = request.params.uid;
     var day = request.params.day;
-    var UserDayStudys = mongoose.model(uid, dayStudys);
 
+    var Student = mongoose.model(uid, student);
     var queryParam = {};
     queryParam['day']=day;
-
-    UserDayStudys.find(queryParam).exec(function(error, result){
-        console.log('----휴----');
-        if(error){
-            console.log(error);
-        }else{
-            console.log(result);
-           // response.send(userstudys);    // echo the result back
-        }
-    });
-});
-
-var student = mongoose.Schema({
-    name : 'string',
-    address : 'string',
-    age : 'number'
-});
-var Student = mongoose.model('Schema', student);
-
-app.get('/statics',function(request, response){
-var day = 30;
-    var queryParam = {};
-    queryParam['age']=day;
-
+    
     Student.find(queryParam).exec(function(error, students){
         console.log('--- Read all ---');
         if(error){
@@ -112,10 +96,5 @@ var day = 30;
         }
     })
 });
-
-
-app.use('/',(req,res,next) => {
-  res.send('hi ssl...ㅇㅓ 왔니...?')
-})
 
 http.createServer(app).listen(80)
